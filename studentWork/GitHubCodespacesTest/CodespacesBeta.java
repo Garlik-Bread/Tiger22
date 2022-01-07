70f13a235bda20a7ab1fe19034e235be80efed2a
Garlik-Bread <hartmjac000@ephrataschools.org>
2022-01-07 18:36:30 +0000

    Modified CodespacesTest.java

--- studentWork/GitHubCodespacesTest/CodespacesBeta.java
+++ studentWork/GitHubCodespacesTest/CodespacesBeta.java
@@ -2,7 +2,7 @@ package studentWork.GitHubCodespacesTest;
 
 public class CodespacesBeta {
     public static void main(String[] args) {
-        String x = "It's a New Year !!!";
+        String x = "2022 is the Year of the TIGER!";
         System.out.println(x);
         
     }
