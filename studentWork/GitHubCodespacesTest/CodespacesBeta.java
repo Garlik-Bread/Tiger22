c6dc4be5256cee28b5f2e00cac827d032b14a30b
Garlik-Bread <hartmjac000@ephrataschools.org>
2022-01-07 18:31:39 +0000

    Modified CodespacesTest.java

---
+++ studentWork/GitHubCodespacesTest/CodespacesBeta.java
@@ -0,0 +1,9 @@
+package studentWork.GitHubCodespacesTest;
+
+public class CodespacesBeta {
+    public static void main(String[] args) {
+        String x = "It's a New Year !!!";
+        System.out.println(x);
+        
+    }
+}
