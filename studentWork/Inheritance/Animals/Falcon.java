55a6699cf4917814db46fcdaf0027fbceaf906a6
GarlikBread <90467683+Garlik-Bread@users.noreply.github.com>
2022-03-25 15:21:22 +0000

    k

---
+++ studentWork/Inheritance/Animals/Falcon.java
@@ -0,0 +1,7 @@
+package studentWork.Inheritance.Animals;
+
+public class Falcon extends Bird {
+   public Falcon() {
+      super("rabbit", 240.0);
+   }
+}
---
+++ studentWork/Inheritance/Animals/Goose.java
@@ -0,0 +1,7 @@
+package studentWork.Inheritance.Animals;
+
+public class Goose extends Bird {
+   public Goose() {
+      super("seeds", 85.0);
+   }
+}
